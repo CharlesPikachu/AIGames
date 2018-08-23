55cae8c629758437922a59305801d92f27479942
CharlesPikachu <1159254961@qq.com>
2018-08-23 22:48:16 +0800

    UPDATE

--- AITRexRush/Algorithm_1/train.py
+++ AITRexRush/Algorithm_1/train.py
@@ -7,6 +7,27 @@ from utils.utils import *
 
 
 # for keras
+# options = {
+# 				"lr": 1e-4,
+# 				"batch_size": 32,
+# 				"max_batch": 150000,
+# 				"input_shape": (96, 96, 4),
+# 				"num_actions": 2,
+# 				"savename": "autodino",
+# 				"savepath": "./model",
+# 				"log_dir": "logger",
+# 				"data_dir": "data",
+# 				"start_prob_jump": 0,
+# 				"end_prob_jump": 0,
+# 				"interval_prob": 1e5,
+# 				"save_interval": 500,
+# 				"num_operations": 200,
+# 				"data_memory": 50000,
+# 				"rd_gamma": 0.99,
+# 				"use_canny": False,
+# 				"num_sampling": 200,
+# 				"game_url": 'chrome://dino'
+# 			}
 options = {
 				"lr": 1e-4,
 				"batch_size": 32,
@@ -17,10 +38,10 @@ options = {
 				"savepath": "./model",
 				"log_dir": "logger",
 				"data_dir": "data",
-				"start_prob_jump": 0,
-				"end_prob_jump": 0,
+				"start_prob_jump": 0.1,
+				"end_prob_jump": 1e-4,
 				"interval_prob": 1e5,
-				"save_interval": 500,
+				"save_interval": 1500,
 				"num_operations": 200,
 				"data_memory": 50000,
 				"rd_gamma": 0.99,
